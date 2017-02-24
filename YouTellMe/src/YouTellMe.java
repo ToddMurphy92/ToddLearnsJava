20586f0ac238174833acff683b2cc2f5d457a994
Todd Murphy <t.murphy@aerocare.com.au>
2017-02-24 20:53:42 +1100

    Created project - YouTellMe

---
+++ YouTellMe/src/YouTellMe.java
@@ -0,0 +1,24 @@
+/**
+ * Created by todd on 24/2/17.
+ */
+public class YouTellMe {
+
+    public static void main (String [] args){
+
+        // Variables
+
+        // Get numbers per line
+        System.out.println("How many numbers per line would you like to print?");
+
+        // Get lines
+        System.out.print("How many lines would you like to print?");
+
+        // Print numbers
+        
+
+        // All done
+        System.out.println("All done");
+
+    }
+
+}
