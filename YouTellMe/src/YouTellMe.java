7908700115f7d1e4b61875017d8bdcca7c8d2483
Todd Murphy <t.murphy@aerocare.com.au>
2017-02-25 11:13:28 +1000

    Project - YouTellMe - Removed comma from end of lines

--- YouTellMe/src/YouTellMe.java
+++ YouTellMe/src/YouTellMe.java
@@ -11,19 +11,20 @@ public class YouTellMe {
         //int countNumbers = 5;
         //int countLines = 5;
         // User Variables
-        int desiredNumbers = 0;
-        int desiredLines = 0;
+        // int desiredNumbers = 0;
+        // int desiredLines = 0;
         int number = 0;
         int printNumber = 0;
-        //int a = 0;
 
+        Scanner input = new Scanner(System.in);
 
         // Get numbers per line
         System.out.println("How many numbers per line would you like to print?");
-        //int desiredLines = a.nextInt():
+        int desiredLines = input.nextInt();
 
         // Get lines
         System.out.println("How many lines would you like to print?");
+        int desiredNumbers = input.nextInt();
 
         // Print numbers
         // For statement that helps print lines
@@ -32,7 +33,11 @@ public class YouTellMe {
             // For statement at creates and prints individual lines
             for (int countNumbers = 1; countNumbers <= desiredNumbers; countNumbers++){
                 printNumber = countNumbers + number;
-                System.out.print(printNumber + ",");
+                System.out.print(printNumber);
+
+                if (printNumber < number + desiredNumbers){
+                    System.out.print(", ");
+                }
             }
 
             System.out.println();
