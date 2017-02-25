ba9d626af83f544d8f176f7ff576c95be8720c69
Todd Murphy <t.murphy@aerocare.com.au>
2017-02-25 10:16:20 +1000

    Completed project draft - YouTellMe

--- YouTellMe/src/YouTellMe.java
+++ YouTellMe/src/YouTellMe.java
@@ -5,16 +5,37 @@ public class YouTellMe {
 
     public static void main (String [] args){
 
-        // Variables
+        // System Variables
+        //int countNumbers = 5;
+        //int countLines = 5;
+        // User Variables
+        int desiredNumbers = 5;
+        int desiredLines = 5;
+        int number = 0;
+        int printNumber = 0;
+
 
         // Get numbers per line
         System.out.println("How many numbers per line would you like to print?");
 
         // Get lines
-        System.out.print("How many lines would you like to print?");
+        System.out.println("How many lines would you like to print?");
 
         // Print numbers
-        
+        // For statement that helps print lines
+        for (int countLines = 0; countLines < desiredLines; countLines++){
+
+            // For statement at creates and prints individual lines
+            for (int countNumbers = 1; countNumbers <= desiredNumbers; countNumbers++){
+                printNumber = countNumbers + number;
+                System.out.print(printNumber + ",");
+            }
+
+            System.out.println();
+
+            number = number + desiredNumbers;
+
+        }
 
         // All done
         System.out.println("All done");
