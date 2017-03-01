6552723410fcaea774c209e54e52df15337b901a
Todd Murphy <t.murphy@aerocare.com.au>
2017-03-01 21:56:23 +1000

    More work on StringsMakeSense.java

--- StringsMakeSense/src/StringsMakeSense.java
+++ StringsMakeSense/src/StringsMakeSense.java
@@ -5,7 +5,21 @@ public class StringsMakeSense {
 
     public static void main (String [] args) {
 
-        
+        String someSaying = new String ("You are unique like everyone else");
+
+        System.out.println(someSaying);
+
+        System.out.println(
+                        someSaying.toLowerCase() + "\n" +
+                        someSaying.toUpperCase() + "\n" +
+                        someSaying.length() + "\n" +
+                        someSaying.charAt(23) + "\n" +
+                        someSaying.charAt(24) + "\n"
+        );
+
+        for (int i=0; i < 4; i++){
+            System.out.print(someSaying.charAt(i));
+        }
 
     }
 
